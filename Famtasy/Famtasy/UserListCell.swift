3c99ea43e2fe70428c7dcd7def6ade5b88984588
Cao Qi <caoqiyeah2014@gmail.com>
2016-03-22 23:28:38 +0800

    UserList is waiting for improving

---
+++ Famtasy/Famtasy/UserListCell.swift
@@ -0,0 +1,14 @@
+//
+//  UserListCell.swift
+//  Famtasy
+//
+//  Created by Jasmine on 16/3/22.
+//  Copyright © 2016年 TAC. All rights reserved.
+//
+
+import UIKit
+
+class UserListCell: UITableViewCell {
+    
+    
+}
